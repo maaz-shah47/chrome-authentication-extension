cf9626bc83144da4df012880657a594a9fd842e3
Maaz Shah <s.tech4747@gmail.com>
2022-07-29 00:21:13 +0500

    Initial Setup and chrome message send

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.runtime.onMessage.addListener((request, response, sendResponse) => {
+  if(request.message === 'login'){
+
+  }else if(request.message === 'logout'){
+
+  }else if(request.message === 'userStatus'){
+    
+  }
+})
